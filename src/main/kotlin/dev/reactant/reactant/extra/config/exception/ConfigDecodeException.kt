5ce65a58a2a6dff8dda96844956cf0af0ccb7097
Setako <zygdtako@outlook.com>
2020-05-14 09:25:11 +0800

    [Fix] Allow access of path exception map

--- src/main/kotlin/dev/reactant/reactant/extra/config/exception/ConfigDecodeException.kt
+++ src/main/kotlin/dev/reactant/reactant/extra/config/exception/ConfigDecodeException.kt
@@ -3,7 +3,7 @@ package dev.reactant.reactant.extra.config.exception
 import dev.reactant.reactant.utils.stackTraceString
 
 class ConfigDecodeException(
-        pathExceptionMap: Map<String, Throwable>
+        val pathExceptionMap: Map<String, Throwable>
 ) : RuntimeException("Error occurred when decoding config:\n ${pathExceptionMap.entries.map { "${it.key}: ${it.value.stackTraceString}" }}") {
 
 }
