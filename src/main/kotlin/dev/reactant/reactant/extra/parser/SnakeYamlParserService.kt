2790901b1ee18250fc412dfdc5e713e3d0b02fc9
Setako <zygdtako@outlook.com>
2020-07-13 23:45:19 +0800

    [Fix] Yaml parser format

--- src/main/kotlin/dev/reactant/reactant/extra/parser/SnakeYamlParserService.kt
+++ src/main/kotlin/dev/reactant/reactant/extra/parser/SnakeYamlParserService.kt
@@ -3,6 +3,7 @@ package dev.reactant.reactant.extra.parser
 import dev.reactant.reactant.core.component.Component
 import dev.reactant.reactant.service.spec.parser.YamlParserService
 import io.reactivex.rxjava3.core.Single
+import org.yaml.snakeyaml.DumperOptions
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor
 import org.yaml.snakeyaml.nodes.Tag
@@ -11,8 +12,14 @@ import kotlin.reflect.KClass
 
 @Component
 class SnakeYamlParserService : YamlParserService {
-    private val representer = Representer()
-    private val yaml = Yaml(CustomClassLoaderConstructor(this.javaClass.classLoader), representer)
+    private val options = DumperOptions().apply {
+        defaultFlowStyle = DumperOptions.FlowStyle.BLOCK
+        indent = 2
+        isPrettyFlow = true
+    }
+    private val representer = Representer(options)
+    private val yaml = Yaml(CustomClassLoaderConstructor(this.javaClass.classLoader), representer, options)
+
     override fun encode(obj: Any): Single<String> =
             Single.defer {
                 representer.addClassTag(obj::class.java, Tag.MAP)
