489066a17470eae639de5814f3087d9b8dee7efa
Setako <zygdtako@outlook.com>
2020-07-31 10:00:33 +0800

    [Change] Allow adding type adapter for default gson parser

--- src/main/kotlin/dev/reactant/reactant/extra/parser/GsonJsonParserService.kt
+++ src/main/kotlin/dev/reactant/reactant/extra/parser/GsonJsonParserService.kt
@@ -1,16 +1,36 @@
 package dev.reactant.reactant.extra.parser
 
 import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapterFactory
 import dev.reactant.reactant.core.component.Component
+import dev.reactant.reactant.core.dependency.injection.components.Components
 import dev.reactant.reactant.service.spec.parser.JsonParserService
 import io.reactivex.rxjava3.core.Single
+import java.lang.reflect.Type
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.jvm.javaType
 
+
+interface TypeAdapterPair {
+    val type: Type;
+
+    /**
+     * Type adapter that will register into GSON
+     * @see GsonBuilder.registerTypeAdapter
+     */
+    val typeAdapter: Any
+}
+
 @Component
-open class GsonJsonParserService : JsonParserService {
-    protected val gson = GsonBuilder().setPrettyPrinting().create()!!
+open class GsonJsonParserService(
+        val typeAdapterFactories: Components<TypeAdapterFactory>,
+        val typeAdapters: Components<TypeAdapterPair>
+) : JsonParserService {
+    protected val gson = GsonBuilder()
+            .also { builder -> typeAdapterFactories.forEach { builder.registerTypeAdapterFactory(it) } }
+            .also { builder -> typeAdapters.forEach { builder.registerTypeAdapter(it.type, it.typeAdapter) } }
+            .setPrettyPrinting().create()!!
 
     override fun encode(obj: Any): Single<String> =
             Single.defer { Single.just(gson.toJson(obj)) }
@@ -24,3 +44,5 @@ open class GsonJsonParserService : JsonParserService {
     override fun <T : Any> decode(encoded: String, modelType: KType): Single<T> =
             Single.defer { Single.just(gson.fromJson<T>(encoded, modelType.javaType)) }
 }
+
+
---
+++ src/main/kotlin/dev/reactant/reactant/extra/parser/gsonadapters/EnumMapGsonTypeAdapterPair.kt
@@ -0,0 +1,21 @@
+package dev.reactant.reactant.extra.parser.gsonadapters
+
+import com.google.gson.InstanceCreator
+import dev.reactant.reactant.core.component.Component
+import dev.reactant.reactant.extra.parser.TypeAdapterPair
+import java.lang.reflect.ParameterizedType
+import java.util.*
+
+@Component
+class EnumMapGsonTypeAdapterPair : TypeAdapterPair {
+    /**
+     * Dummy enum class
+     */
+    enum class X {}
+
+    override val type = EnumMap::class.java
+    override val typeAdapter: InstanceCreator<EnumMap<*, *>> = InstanceCreator {
+        val clazz = ((it as ParameterizedType).actualTypeArguments[0]!! as Class<Any>)
+        return@InstanceCreator EnumMap<X, Any>(clazz as Class<X>)
+    }
+}
